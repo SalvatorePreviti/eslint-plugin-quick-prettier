841b9eb78c97598bc988b5533256001368df45f3
Salvatore Previti <roorback@gmail.com>
2019-06-30 20:27:04 +0100

    simplify helpers

--- README.md
+++ README.md
@@ -58,16 +58,7 @@ const myConfiguration = {
   ]
 }
 
-module.exports = helpers.addEslintConfigPrettierRules(eslintConfig)
-```
-
-## helpers.mergeEslintConfigs
-
-Merges multiple eslint configuration objects together.
-Returns a new configuration object.
-
-```js
-const mergedConfig = mergeEslintConfigs(config1, config2, config3 ...)
+module.exports = helpers.addEslintConfigPrettierRules(myConfiguration)
 ```
 
 ## helpers.getPrettierConfig
@@ -83,36 +74,3 @@ console.log(prettierConfig.printWidth)
 ## helpers.getPrettier
 
 Requires `require("prettier")`, but prefer the version installed in your current directory if present.
-
-## helpers.hasLocalPackage
-
-Returns true if the given module (or submodule file) is a locally installed module or file.
-Returns false if the module does not exists or is installed only globally.
-This function caches the result.
-
-```js
-const isSomeModuleInstalledLocally = helpers.hasLocalPackage('some-module')
-```
-
-## helpers.isGlobalPath
-
-Checks if a path is a global require module path.
-
-```js
-const isGlobalPath = helpers.isGlobalPath(helpers.tryResolveLocal('some-module'))
-console.log('some-module is installed', isGlobalPath ? 'globally' : 'locally')
-```
-
-## helpers.addNodeRequirePath
-
-Adds additional require paths to node module resolve mechanism to allow eslint to load plugins and extends from other folders.
-It does it by overriding the function `require('module')._nodeModulePaths`.
-Called the first time, adds also helpers.baseFolder (that by default is process.cwd()).
-
-```js
-helpers.addNodeRequirePath()
-```
-
-```js
-helpers.addNodeRequirePath('/folter/a/node_modules')
-```
--- eslint-helpers.js
+++ eslint-helpers.js
@@ -1,18 +1,12 @@
-const Module = require('module')
 const path = require('path')
 const fs = require('fs')
+const Module = require('module')
 
-const { isArray, from: arrayFrom } = Array
-const { keys: objectKeys, assign: objectAssign, defineProperty } = Object
-const { existsSync } = fs
+const { isArray } = Array
 
 const _eslintExpectedPath = `node_modules${path.sep}eslint${path.sep}`
-const _hasPackageCache = new Map()
-const _globalPathsArray = Module.globalPaths || []
-const _globalPathsSet = new Set(_globalPathsArray)
 let _prettier = null
 let _prettierConfig
-let _oldNodeModulePaths = null
 
 /**
  * Requires eslint. Does it by understanding which one is the currently running eslint.
@@ -59,7 +53,47 @@ function getCallerEslintApi(callerFunction, stackTraceLimit = 10) {
  * @returns {typeof import('prettier')}
  */
 function getPrettier() {
-  return _prettier || (_prettier = module.exports.tryRequireLocal('prettier') || require('prettier'))
+  return _prettier || (_prettier = requirePrettier())
+}
+
+function requirePrettier() {
+  if (_prettier) {
+    return _prettier
+  }
+
+  const globalPathsSet = new Set(Module.globalPaths || [])
+
+  function isGlobalPath(filepath) {
+    if (typeof filepath === 'string' && filepath.length !== 0) {
+      if (filepath.startsWith(module.exports.baseFolder)) {
+        return false
+      }
+      for (const p of globalPathsSet) {
+        if (filepath.startsWith(p)) {
+          return true
+        }
+      }
+    }
+    return false
+  }
+
+  const resolvePaths = new Set(
+    (
+      (require.resolve && require.resolve.paths && require.resolve.paths(module.exports.baseFolder)) ||
+      Module._nodeModulePaths(module.exports.baseFolder)
+    ).filter(x => !isGlobalPath(x) && fs.existsSync(x))
+  )
+  const thisPackage = path.join(__dirname, 'node_modules')
+  if (fs.existsSync(thisPackage)) {
+    resolvePaths.add(thisPackage)
+  }
+
+  const cwdPaths = require.resolve.paths(path.join(process.cwd(), 'package.json'))
+  for (const p of cwdPaths) {
+    resolvePaths.add(p)
+  }
+
+  return require(require.resolve('prettier', { paths: Array.from(resolvePaths) }))
 }
 
 function getPrettierConfig() {
@@ -73,71 +107,6 @@ function getPrettierConfig() {
     }
   }
   return _prettierConfig
-}
-
-function mergeEslintConfigs(...sources) {
-  let result = {}
-  for (const source of sources) {
-    result = deepmerge(result, source, true)
-  }
-  return result
-}
-
-function deepmerge(target, src, combine, isRule) {
-  /*
-   * This code is inspired from deepmerge and eslint
-   * (https://github.com/KyleAMathews/deepmerge)
-   */
-  const array = isArray(src) || isArray(target)
-  let dst = (array && []) || {}
-
-  if (array) {
-    const resolvedTarget = target || []
-
-    // src could be a string, so check for array
-    if (isRule && Array.isArray(src) && src.length > 1) {
-      dst = dst.concat(src)
-    } else {
-      dst = dst.concat(resolvedTarget)
-    }
-    const resolvedSrc = typeof src === 'object' ? src : [src]
-    const keys = objectKeys(resolvedSrc)
-    for (let i = 0, len = keys.length; i !== len; ++i) {
-      const e = resolvedSrc[i]
-      if (dst[i] === undefined) {
-        dst[i] = e
-      } else if (typeof e === 'object') {
-        if (isRule) {
-          dst[i] = e
-        } else {
-          dst[i] = deepmerge(resolvedTarget[i], e, combine, isRule)
-        }
-      } else if (!combine) {
-        dst[i] = e
-      } else if (dst.indexOf(e) === -1) {
-        dst.push(e)
-      }
-    }
-  } else {
-    if (target && typeof target === 'object') {
-      objectAssign(dst, target)
-    }
-    const keys = objectKeys(src)
-    for (let i = 0, len = keys.length; i !== len; ++i) {
-      const key = keys[i]
-      if (key === 'overrides') {
-        dst[key] = (target[key] || []).concat(src[key] || [])
-      } else if (Array.isArray(src[key]) || Array.isArray(target[key])) {
-        dst[key] = deepmerge(target[key], src[key], key === 'plugins' || key === 'extends', isRule)
-      } else if (typeof src[key] !== 'object' || !src[key] || key === 'exported' || key === 'astGlobals') {
-        dst[key] = src[key]
-      } else {
-        dst[key] = deepmerge(target[key] || {}, src[key], combine, key === 'rules')
-      }
-    }
-  }
-
-  return dst
 }
 
 function addToPluginsSet(set, eslintConfig) {
@@ -187,15 +156,13 @@ function addToPluginsSet(set, eslintConfig) {
 }
 
 function addEslintConfigPrettierRules(eslintConfig) {
-  eslintConfig = deepmerge({}, eslintConfig, true)
+  eslintConfig = { ...eslintConfig }
 
   const pluginSet = new Set()
   addToPluginsSet(pluginSet, eslintConfig)
 
   if (!pluginSet.has('quick-prettier')) {
-    if (!eslintConfig.plugins) {
-      eslintConfig.plugins = []
-    }
+    eslintConfig.plugins = isArray(eslintConfig.plugins) ? [...eslintConfig.plugins] : []
     eslintConfig.plugins.push('quick-prettier')
   }
 
@@ -260,136 +227,8 @@ function addEslintConfigPrettierRules(eslintConfig) {
   return eslintConfig
 }
 
-/**
- * Checks if a path is a global require module path.
- * @param {string|null|undefined} filepath The file path to check
- * @returns {boolean} True if the path is a global node_modules path, false if not.
- */
-function isGlobalPath(filepath) {
-  if (typeof filepath === 'string' && filepath.length !== 0) {
-    if (filepath.startsWith(module.exports.baseFolder)) {
-      return false
-    }
-    if (_globalPathsSet.has(filepath)) {
-      return true
-    }
-    for (let i = 0; i < _globalPathsArray.length; ++i) {
-      if (filepath.startsWith(_globalPathsArray[i])) {
-        return true
-      }
-    }
-  }
-  return false
-}
-
-let _resolvePaths
-
-function getResolvePathsSet() {
-  if (!_resolvePaths) {
-    _resolvePaths = new Set(
-      (
-        (require.resolve && require.resolve.paths && require.resolve.paths(module.exports.baseFolder)) ||
-        Module._nodeModulePaths(module.exports.baseFolder)
-      ).filter(x => !isGlobalPath(x) && existsSync(x))
-    )
-    const thisPackage = path.join(__dirname, 'node_modules')
-    if (existsSync(thisPackage)) {
-      _resolvePaths.add(thisPackage)
-    }
-  }
-  return _resolvePaths
-}
-
-/**
- * Tries to resolve the path of a module.
- * @param {string} id The module to resolve.
- * @returns {string|null} The resolved module path or null if not found.
- */
-function hasLocalPackage(id) {
-  if (id.startsWith('.')) {
-    id = path.resolve(module.exports.baseFolder, id)
-  } else if (id.startsWith(path.sep) || id.startsWith('/')) {
-    id = path.resolve(id)
-  }
-  let result = _hasPackageCache.get(id)
-  if (result === undefined) {
-    result = false
-    try {
-      if (isGlobalPath(require.resolve(id))) {
-        result = true
-      }
-    } catch (_error) {}
-    _hasPackageCache.set(id, result)
-  }
-  return result
-}
-
-function _nodeModulePaths(from) {
-  const set = new Set()
-  let customAdded = false
-  const defaults = _oldNodeModulePaths.call(Module, from)
-
-  for (let i = 0, defaultsLen = defaults.length; i !== defaultsLen; ++i) {
-    const value = defaults[i]
-    if (!customAdded && _globalPathsSet.has(value)) {
-      customAdded = true
-      for (const p of _resolvePaths || getResolvePathsSet()) {
-        set.add(p)
-      }
-    }
-    set.add(defaults[i])
-  }
-  if (!customAdded) {
-    for (const p of _resolvePaths || getResolvePathsSet()) {
-      set.add(p)
-    }
-  }
-  return arrayFrom(set)
-}
-
-function addNodeRequirePath(additionalPath) {
-  if (_oldNodeModulePaths === null) {
-    _oldNodeModulePaths = Module._nodeModulePaths
-    if (typeof _oldNodeModulePaths !== 'function') {
-      throw new Error(
-        'Module._nodeModulePaths is undefined. Maybe node version ' + process.version + ' does not support it?'
-      )
-    }
-    Module._nodeModulePaths = _nodeModulePaths
-  }
-
-  if (additionalPath) {
-    getResolvePathsSet().add(path.resolve(additionalPath))
-  }
-}
-
-function defineLazyProperty(target, name, getter) {
-  defineProperty(target, name, {
-    get() {
-      const result = getter()
-      this[name] = result
-      return result
-    },
-    set(value) {
-      defineProperty(target, name, {
-        value,
-        configurable: true,
-        writable: true,
-        enumerable: true
-      })
-    },
-    configurable: true,
-    enumerable: true
-  })
-}
-
-exports.baseFolder = process.cwd()
-exports.hasLocalPackage = hasLocalPackage
-exports.isGlobalPath = isGlobalPath
-exports.getPrettier = getPrettier
-exports.getPrettierConfig = getPrettierConfig
-exports.getCallerEslintApi = getCallerEslintApi
-exports.mergeEslintConfigs = mergeEslintConfigs
-exports.addEslintConfigPrettierRules = addEslintConfigPrettierRules
-exports.addNodeRequirePath = addNodeRequirePath
-exports.defineLazyProperty = defineLazyProperty
+module.exports.baseFolder = process.cwd()
+module.exports.getPrettier = getPrettier
+module.exports.getPrettierConfig = getPrettierConfig
+module.exports.getCallerEslintApi = getCallerEslintApi
+module.exports.addEslintConfigPrettierRules = addEslintConfigPrettierRules
