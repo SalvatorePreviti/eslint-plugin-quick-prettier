930809f65284ab9d7548a4a51fb3e0ca9aa8b973
Salvatore Previti <roorback@gmail.com>
2019-08-07 13:43:29 +0100

    minor

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ module.exports = {
             ]
           }
         ],
-        'no-tabs': 1,
+        'no-tabs': 0,
         'no-unexpected-multiline': 1,
         'quick-prettier/prettier': [
           1,
