0a64e4db6b8ae60b3bbca9380a6a61e26576ddb8
Salvatore Previti <roorback@gmail.com>
2019-08-09 17:26:46 +0100

    single-responsability

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,12 @@
-const eslintPluginQuickPrettier = require('./')
-
-const rules = {
-  ...eslintPluginQuickPrettier.configs.recommended.rules,
-  'no-console': 1,
-  'no-empty': 0,
-  'no-confusing-arrow': [2, { allowParens: false }]
-}
-
-const quickPrettierRules = rules['quick-prettier/prettier']
-delete rules['quick-prettier/prettier']
-rules['self/prettier'] = quickPrettierRules
-
 module.exports = {
-  extends: ['plugin:eslint-plugin/recommended', 'plugin:node/recommended', 'eslint:recommended'],
+  extends: ['prettier', 'plugin:eslint-plugin/recommended', 'plugin:node/recommended', 'eslint:recommended'],
   plugins: ['eslint-plugin', 'json', 'self'],
   env: { mocha: true },
   root: true,
-  rules
+  rules: {
+    'self/prettier': 1,
+    'no-console': 1,
+    'no-empty': 0,
+    'no-confusing-arrow': [2, { allowParens: false }]
+  }
 }
